551a49608d90f43340bf214fdd9be230ad1206f2
Luciano Scarpulla <scarpulla53@gmail.com>
2025-02-12 18:12:31 +0800

    use .rows method

--- src/reflex_plot/core.py
+++ src/reflex_plot/core.py
@@ -1,23 +1,8 @@
-from typing import Any, Dict, Literal, Optional
+from typing import Dict, Literal, Optional
 
 import narwhals as nw
 import reflex as rx
 from narwhals.typing import IntoDataFrame
-
-
-def key_value_to_dicts(data_dict: dict[str, list[Any]]) -> list[dict[str, Any]]:
-    """Convert dictionary form {key1:[v1,v2], key2:[v3,v4]} into [{key1:v1,key2:v3},{key1:v2,key2:v4}]."""
-    result = []
-    max_length = max(len(values) for values in data_dict.values())
-
-    for i in range(max_length):
-        entry = {}
-        for key, values in data_dict.items():
-            if i < len(values):
-                entry[key] = values[i]
-        result.append(entry)
-
-    return result
 
 
 def plot(
@@ -34,7 +19,7 @@ def plot(
     margin: Dict[str, int] = dict(left=60, bottom=60),
 ) -> rx.Component:
     df = nw.from_native(df)
-    data = key_value_to_dicts(df.to_dict(as_series=False))
+    data = df.rows(named=True)
     components = [
         getattr(rx.recharts, kind)(data_key=y),
         rx.recharts.x_axis(x or xlabel, label=dict(value=x, position="bottom")),
